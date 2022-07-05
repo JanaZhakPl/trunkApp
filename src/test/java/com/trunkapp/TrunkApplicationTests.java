c4832f3b9f47be7b89090fb710f7d9fa8df8f85e
JanaZhakPL <janazhakpl@gmail.com>
2022-07-05 02:45:40 +0200

    tms final project

---
+++ src/test/java/com/trunkapp/TrunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trunkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrunkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
